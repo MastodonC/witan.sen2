c72574b06e3809bd59dae54a33d05d3159618fd7
eraderna <andrew.holmes@mastodonc.com>
2023-12-15 17:34:35 +0000

    Update SEN2 caseload extraction
    - Convert to kebab-case column names
    - Read in caseload figures as integers, "z" as nil.

--- src/witan/sen2/ehcp_stats.clj
+++ src/witan/sen2/ehcp_stats.clj
@@ -1,20 +1,31 @@
 (ns witan.sen2.ehcp-stats
   "GOV.UK EHCP stats from https://explore-education-statistics.service.gov.uk/find-statistics/education-health-and-care-plans"
   (:require [clojure.java.io :as io]
+            [clojure.string :as string]
             [tablecloth.api :as tc]))
 
 (def caseload (delay (-> "sen2_estab_caseload.csv"
                          io/resource
                          io/file
-                         (tc/dataset {:key-fn keyword})
-                         (tc/select-rows #(and (= (:establishment_group %) "Total")
-                                               (= (:ehcp_or_statement %) "Total")))
-                         (tc/select-columns [:num_caseload :time_period
-                                             :new_la_code :la_name])))) ;; this doesn't work with districts
+                         (tc/dataset {:key-fn    (comp keyword #(string/replace % #"_" "-"))
+                                      :parser-fn {:num-caseload [:int32 #(if (= "z" %)
+                                                                           :tech.v3.dataset/missing
+                                                                           (parse-long %))]}})
+                         (tc/select-rows #(and (= (:establishment-group %) "Total")
+                                               (= (:ehcp-or-statement   %) "Total")))
+                         (tc/select-columns [:new-la-code
+                                             :old-la-code
+                                             :la-name
+                                             :time-period
+                                             :num-caseload])
+                         )
+
+                     )
+  ) ;; this doesn't work with districts
 
 (defn generate-current-pop [s la-or-gss]
   "Returns total EHCP caseload for LA `s` specified by name (when `la-or-gss` is `:la`) or GSS code (when `la-or-gss` is `:gss`)"
   (let [pred (cond
-               (= la-or-gss :gss) (fn [x] (= (:new_la_code x) s))
-               (= la-or-gss :la ) (fn [x] (= (:la_name     x) s)))]
+               (= la-or-gss :gss) (fn [x] (= (:new-la-code x) s))
+               (= la-or-gss :la ) (fn [x] (= (:la-name     x) s)))]
     (tc/select-rows @caseload #(pred %))))
