a698978de4c9b4059c0c57eee3f959b2d2c5678d
Seb Bailey <seb231@me.com>
2026-06-12 13:38:56 +0100

    pre-submission processing with issues

--- src/witan/sen2/plans_placements_with_issues.clj
+++ src/witan/sen2/plans_placements_with_issues.clj
@@ -2,6 +2,7 @@
   (:require [witan.sen2 :as sen2]
             [witan.sen2.return.person-level.blade.csv :as sen2-blade-csv]
             [witan.sen2.return.person-level.blade.plans-placements :as plans]
+            [witan.sen2.return.person-level.blade.pre-submission :as pre]
             [tablecloth.api :as tc]
             [kixi.large :as large]))
 
@@ -30,6 +31,45 @@
                           (plans/plans-placements-on-census-dates sen2-blade-csv-ds-map
                                                                   census-dates-ds))]
     (as-> {:blade-csv-ds-map sen2-blade-csv-ds-map
+           :census-raw (if updates-file
+                         (as-> updates-file $
+                           (plans/updates-csv-file->ds $)
+                           (plans/update-plans-placements-on-census-dates sen2-census-raw $))
+                         sen2-census-raw)
+           :checks checks
+           :census-dates census-dates-ds} $
+      (assoc $ :issues (plans/issues->ds (:census-raw $) checks))
+      (assoc $ :issues-summary (-> (:issues $)
+                                   (plans/issues->ds checks)
+                                   (plans/summarise-issues checks))))))
+
+(defn raw-sen2-pre-submission->plans-placements-with-issues
+  [{:keys [census-years data-path module-read-cfg
+           updates-file manually-updated-sen2 checks
+           sheet-names]
+    :or {module-read-cfg sen2-blade-csv/module-read-cfg
+         checks (plans/checks)}}]
+  "Process raw SEN2 data or apply updates/fixes to SEN2 data. Expects a map with required and optional keys.
+   Required key-values:
+   - `:census-years`: vector of calendar years covered in this SEN2 data
+   - `:data-path`: location of SEN2 xlsx pre-submission file
+   - `:sheet-names`: map of required dataset keywords to sheet/tab names in workbook
+   Optional extra key-values:
+   - `:updates-file`: filepath of file with fixes/updates to records - usually created by manually updating
+      the output of `write-out-issues`
+   - `:manually-updated-sen2`: an updated SEN2 ds to be used where fixes are too complicated to be applied
+      via an updates files (e.g. blanket fixes to RP/SENU labelling)
+   - `module-read-cfg`: see `sen2-blade-csv/module-read-cfg` for applying bespoke SEN2 module parsing, otherwise defaults
+   - `:checks`: see `plans/checks` for applying bespoke SEN2 issue checks, otherwise defaults"
+  (let [sen2-ds-map (pre/pre-submission-workbook-filepath->ds-map data-path
+                                                                  {:module-sheet-names sheet-names
+                                                                   :module-read-cfg    module-read-cfg})
+        census-dates-ds (sen2/census-years->census-dates-ds census-years)
+        sen2-census-raw (if manually-updated-sen2
+                          manually-updated-sen2
+                          (plans/plans-placements-on-census-dates sen2-ds-map
+                                                                  census-dates-ds))]
+    (as-> {:blade-csv-ds-map sen2-ds-map
            :census-raw (if updates-file
                          (as-> updates-file $
                            (plans/updates-csv-file->ds $)
